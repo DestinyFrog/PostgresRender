019dea8f828999be7392168b6734530f2c078bb1
pontus <pontus@pontusUbuntuDesktop.myguest.virtualbox.org>
2024-05-21 09:23:53 -0300

    logs

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,21 @@
 import pg from 'pg'
 
 ( async () => {
+    console.log("Start")
+
     const { Client } = pg
     const client = new Client({
         user: "carve",
         password: "K1cN4tUGvOCU0MdUsCjjaKYk3wtMAgU9",
-        database: "Liba",
+        database: "liba",
         port: 5432,
         host: "dpg-cp5pdf2cn0vc73bj7g4g-a"
     })
     await client.connect()
     
-    const res = await client.query('SELECT $1::text as message', ['Hello world!'])
+    const res = await client.query('SELECT NOW()')
     console.log(res.rows[0].message)
     await client.end()
+
+    console.log("End !!")
 } ).call(this)
